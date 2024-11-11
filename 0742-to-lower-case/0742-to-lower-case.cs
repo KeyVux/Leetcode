00676fd038cf3bb801a0b7ea4110aa6c656e87ca
KeyVux <99638998+KeyVux@users.noreply.github.com>
2024-11-11 23:14:15 +0700

    Time: 0 ms (100%), Space: 38.7 MB (87.06%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.cs
+++ 0742-to-lower-case/0742-to-lower-case.cs
@@ -1,5 +1,22 @@
 public class Solution {
     public string ToLowerCase(string s) {
-        return s.ToLower();
+        
+        //return s.ToLower();
+        
+        StringBuilder resultString = new StringBuilder();
+
+        for (int sIndex = 0; sIndex < s.Length; sIndex++)
+        {
+            char charIndex = s[sIndex];
+            if (charIndex >= 'A' && charIndex <= 'Z')
+            {
+                resultString.Append((char)(charIndex + 32));
+            }
+            else
+            {
+                resultString.Append(charIndex);
+            }
+        }
+        return resultString.ToString();
     }
 }
