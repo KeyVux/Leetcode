3d22f758539df2133a37a3160109ba313f8e0a43
KeyVux <99638998+KeyVux@users.noreply.github.com>
2024-11-11 22:42:27 +0700

    Time: 1 ms (100%), Space: 42.3 MB (67.2%) - LeetHub

---
+++ 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int MostWordsFound(string[] sentences) {
+        int maxWords = 0; 
+
+        foreach (string sentence in sentences) 
+        {
+            int wordCount = sentence.Split(' ').Length;
+
+            if (wordCount > maxWords) 
+            {
+                maxWords = wordCount;
+            }
+        }
+
+        return maxWords; 
+    }
+}
