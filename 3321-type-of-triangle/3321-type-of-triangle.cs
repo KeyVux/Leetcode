553acb423c3dccd9ce89958a097819c09c4d5006
KeyVux <99638998+KeyVux@users.noreply.github.com>
2024-11-11 23:32:43 +0700

    Time: 0 ms (100%), Space: 43.6 MB (76.19%) - LeetHub

---
+++ 3321-type-of-triangle/3321-type-of-triangle.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public string TriangleType(int[] nums) {
+        if (nums[0] == nums[1] && nums[0] == nums[2] )
+        {
+            return "equilateral";
+        }
+        if (nums[0] + nums[1] > nums [2]
+            && nums[1] + nums[2] > nums [0]
+            && nums[2] + nums[0] > nums [1]
+        )
+        {
+            if (nums[0] == nums[1] || nums[0] == nums[2] || nums[1] == nums[2])
+            {
+                return "isosceles";
+            }
+
+            return "scalene";
+        }
+        return "none";
+    }
+}
