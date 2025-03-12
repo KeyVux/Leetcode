83deb50e56c8f592e5d3d537efbdc83e24dd608c
KeyVux <99638998+KeyVux@users.noreply.github.com>
2025-03-13 03:26:20 +0700

    Time: 2 ms (25.38%), Space: 39.2 MB (36.85%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cs
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cs
@@ -1,18 +1,18 @@
 public class Solution {
 public int MinimumRecolors(string blocks, int k) {
-    int minOperations = int.MaxValue;  // Start with the largest possible value
+    int minOperations = int.MaxValue;  // Start with a large value
 
     for (int i = 0; i <= blocks.Length - k; i++) {
         int countW = 0;
 
-        // Count the number of 'W' in the current window of size k
+        // Count 'W' in the current window of size k
         for (int j = i; j < i + k; j++) {
-            if (blocks[j] == 'W') {
+            if (blocks[j] == 'W') {  
                 countW++;
             }
         }
 
-        minOperations = Math.Min(minOperations, countW); // Update if we find a smaller count
+        minOperations = Math.Min(minOperations, countW); // Update with the minimum
     }
 
     return minOperations;
